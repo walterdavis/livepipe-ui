eef2ff4d5c651b83723e083578296104cba5eff9
Stuart Halloway <stu@thinkrelevance.com>
2008-07-23 13:39:34 -0400

    characterizing setCookie

--- spec/cookie_spec.js
+++ spec/cookie_spec.js
@@ -2,14 +2,31 @@ load("jsspec/config.js");
 load("src/cookie.js");
 
 with(Spec) {
-  describe("Cookie", function() {with(this) {
-    it("should set a cookie", function() {
+  describe("Cookie set", function() {with(this) {
+    it("should set a cookie with an expiration", function() {
       Cookie.set("foo", "value", 60);
-      //TODO: check something!
+      var cookie = document.cookie.split("; ")
+      cookie.length.should(equal(3));
+      cookie[0].should(equal("foo=value"));
+      cookie[1].should(match(/^expires=\w+, \d+ \w+ \d\d\d\d \d\d:\d\d:\d\d \w+$/));
+      cookie[2].should(equal("path=/"));
     });
-
+    it("should set a cookie without an expiration", function() {
+      Cookie.set("foo", "value");
+      var cookie = document.cookie.split("; ")
+      cookie.length.should(equal(2));
+      cookie[0].should(equal("foo=value"));
+      cookie[1].should(equal("path=/"));
+    });
+    it("should trigger a 'set' event ", function() {
+      var eventCounter = 0;
+      Cookie.observeOnce("set", function() {
+        eventCounter++;  
+      });
+      Cookie.set("foo", "value");
+      eventCounter.should(equal(1));
+    });
   }});
-
 }
 
 Specs.report = "ConsoleReport";
