974d88cc192d615656182471318285e5b7aa393e
Stuart Halloway <stu@thinkrelevance.com>
2008-07-23 14:35:48 -0400

    refactored get to use regular expressions

--- src/cookie.js
+++ src/cookie.js
@@ -28,16 +28,8 @@ var Cookie = {
   },
   get: function(name){
     Cookie.notify('get',name);
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++){
-      var c = ca[i];
-      while(c.charAt(0) == ' ')
-        c = c.substring(1,c.length);
-      if(c.indexOf(nameEQ) == 0)
-        return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+    var valueMatch = new RegExp(name + "=([^;]+)").exec(document.cookie);
+    return valueMatch ? valueMatch[1] : null;
   },
   unset: function(name){
     Cookie.notify('unset',name);
