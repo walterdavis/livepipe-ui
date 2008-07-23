94212dc0de25fc7fea465c10f546a818d30e289b
Stuart Halloway <stu@thinkrelevance.com>
2008-07-23 09:39:27 -0400

    minimally loading livepipe and running a no-op spec

--- jsspec/config.js
+++ jsspec/config.js
@@ -1,5 +1,6 @@
 load("jsspec/env.js");
 window.location = "spec/index.html";
 load("lib/prototype.js");
+load("src/livepipe.js");
 load("jsspec/jsspec.js");
 
--- spec/cookie_spec.js
+++ spec/cookie_spec.js
@@ -1,2 +1,16 @@
 load("jsspec/config.js");
+load("src/cookie.js");
 
+with(Spec) {
+  describe("Cookie", function() {with(this) {
+    it("should set a cookie", function() {
+      Cookie.set("foo", "value", 60);
+      //TODO: check something!
+    });
+
+  }});
+
+}
+
+Specs.report = "ConsoleReport";
+Specs.run();
