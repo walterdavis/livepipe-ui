482b1151785aac2f924641a6b25dd0f4097887ab
Nathan Lloyd Smith <nlloyds@gmail.com>
2009-05-25 22:12:38 -0500

    cookie.js lint

--- src/cookie.js
+++ src/cookie.js
@@ -7,10 +7,12 @@
  * @attribution http://www.quirksmode.org/js/cookies.html
  */
 
-if(typeof(Prototype) == "undefined")
-  throw "Cookie requires Prototype to be loaded."
-if(typeof(Object.Event) == "undefined")
-  throw "Cookie requires Object.Event to be loaded.";
+/*global document, Prototype, $A */
+
+if(typeof(Prototype) == "undefined") {
+  throw "Cookie requires Prototype to be loaded."; }
+if(typeof(Object.Event) == "undefined") {
+  throw "Cookie requires Object.Event to be loaded."; }
 
 var Cookie = {
   build: function() {
