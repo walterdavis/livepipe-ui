d6f201f3d8cf950814c711209f726e6322472c17
Nathan Lloyd Smith <nlloyds@gmail.com>
2009-05-30 22:06:22 -0500

    tabs.js lint

--- src/tabs.js
+++ src/tabs.js
@@ -7,15 +7,17 @@
  * @require prototype.js, livepipe.js
  */
 
-if(typeof(Prototype) == "undefined")
-    throw "Control.Tabs requires Prototype to be loaded.";
-if(typeof(Object.Event) == "undefined")
-    throw "Control.Tabs requires Object.Event to be loaded.";
+/*global window, document, Prototype, $, $A, $H, $break, Class, Element, Event, Control */
+
+if(typeof(Prototype) == "undefined") {
+    throw "Control.Tabs requires Prototype to be loaded."; }
+if(typeof(Object.Event) == "undefined") {
+    throw "Control.Tabs requires Object.Event to be loaded."; }
 
 Control.Tabs = Class.create({
     initialize: function(tab_list_container,options){
-        if(!$(tab_list_container))
-            throw "Control.Tabs could not find the element: " + tab_list_container;
+        if(!$(tab_list_container)) {
+            throw "Control.Tabs could not find the element: " + tab_list_container; }
         this.activeContainer = false;
         this.activeLink = false;
         this.containers = $H({});
@@ -35,21 +37,21 @@ Control.Tabs = Class.create({
             hideFunction: Element.hide
         };
         Object.extend(this.options,options || {});
-        (typeof(this.options.linkSelector == 'string')
-            ? $(tab_list_container).select(this.options.linkSelector)
-            : this.options.linkSelector($(tab_list_container))
+        (typeof(this.options.linkSelector == 'string') ? 
+            $(tab_list_container).select(this.options.linkSelector) : 
+            this.options.linkSelector($(tab_list_container))
         ).findAll(function(link){
             return (/^#/).exec((Prototype.Browser.WebKit ? decodeURIComponent(link.href) : link.href).replace(window.location.href.split('#')[0],''));
         }).each(function(link){
             this.addTab(link);
         }.bind(this));
         this.containers.values().each(Element.hide);
-        if(this.options.defaultTab == 'first')
+        if(this.options.defaultTab == 'first') {
             this.setActiveTab(this.links.first());
-        else if(this.options.defaultTab == 'last')
+        } else if(this.options.defaultTab == 'last') {
             this.setActiveTab(this.links.last());
-        else
-            this.setActiveTab(this.options.defaultTab);
+        } else {
+            this.setActiveTab(this.options.defaultTab); }
         var targets = this.options.targetRegExp.exec(window.location);
         if(targets && targets[1]){
             targets[1].split(',').each(function(target){
@@ -77,19 +79,19 @@ Control.Tabs = Class.create({
         this.links.push(link);
         link.key = link.getAttribute('href').replace(window.location.href.split('#')[0],'').split('#').last().replace(/#/,'');
         var container = $(link.key);
-        if(!container)
-            throw "Control.Tabs: #" + link.key + " was not found on the page."
+        if(!container) {
+            throw "Control.Tabs: #" + link.key + " was not found on the page."; }
         this.containers.set(link.key,container);
         link[this.options.hover ? 'onmouseover' : 'onclick'] = function(link){
-            if(window.event)
-                Event.stop(window.event);
+            if(window.event) {
+                Event.stop(window.event); }
             this.setActiveTab(link);
             return false;
         }.bind(this,link);
     },
     setActiveTab: function(link){
-        if(!link && typeof(link) == 'undefined')
-            return;
+        if(!link && typeof(link) == 'undefined') {
+            return; }
         if(typeof(link) == 'string'){
             this.setActiveTab(this.links.find(function(_link){
                 return _link.key == link;
@@ -97,10 +99,10 @@ Control.Tabs = Class.create({
         }else if(typeof(link) == 'number'){
             this.setActiveTab(this.links[link]);
         }else{
-            if(this.notify('beforeChange',this.activeContainer,this.containers.get(link.key)) === false)
-                return;
-            if(this.activeContainer)
-                this.options.hideFunction(this.activeContainer);
+            if(this.notify('beforeChange',this.activeContainer,this.containers.get(link.key)) === false) {
+                return; }
+            if(this.activeContainer) {
+                this.options.hideFunction(this.activeContainer); }
             this.links.each(function(item){
                 (this.options.setClassOnContainer ? $(item.parentNode) : item).removeClassName(this.options.activeClassName);
             }.bind(this));
