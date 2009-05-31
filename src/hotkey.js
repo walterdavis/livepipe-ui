14df3c440957be11b9d0199b54b1cce51b97a86f
Nathan Lloyd Smith <nlloyds@gmail.com>
2009-05-30 21:16:09 -0500

    Replaced tabs with spaces

--- src/hotkey.js
+++ src/hotkey.js
@@ -10,23 +10,23 @@
 /*global document, Prototype, Class, Event, $ */
 
 if(typeof(Prototype) == "undefined") {
-	throw "HotKey requires Prototype to be loaded."; }
+    throw "HotKey requires Prototype to be loaded."; }
 if(typeof(Object.Event) == "undefined") {
-	throw "HotKey requires Object.Event to be loaded."; }
+    throw "HotKey requires Object.Event to be loaded."; }
 
 var HotKey = Class.create({
-	initialize: function(letter,callback,options){
-		letter = letter.toUpperCase();
-		HotKey.hotkeys.push(this);
-		this.options = Object.extend({
-			element: false,
-			shiftKey: false,
-			altKey: false,
-			ctrlKey: true,
+    initialize: function(letter,callback,options){
+        letter = letter.toUpperCase();
+        HotKey.hotkeys.push(this);
+        this.options = Object.extend({
+            element: false,
+            shiftKey: false,
+            altKey: false,
+            ctrlKey: true,
             bubbleEvent : true,
             fireOnce : false // Keep repeating event while key is pressed?
-		},options || {});
-		this.letter = letter;
+        },options || {});
+        this.letter = letter;
 
         // All custom hotkey events should stop after their custom actions.
         this.callback = function (event) {
@@ -37,38 +37,38 @@ var HotKey = Class.create({
             this.fired = true;
         };
 
-		this.element = $(this.options.element || document);
-		this.handler = function(event){
-			if(!event || (
-				(Event['KEY_' + this.letter] || this.letter.charCodeAt(0)) == event.keyCode &&
-				((!this.options.shiftKey || (this.options.shiftKey && event.shiftKey)) &&
-					(!this.options.altKey || (this.options.altKey && event.altKey)) &&
-					(!this.options.ctrlKey || (this.options.ctrlKey && event.ctrlKey))
-				)
-			)){
-				if(this.notify('beforeCallback',event) === false) {
-					return; }
-				this.callback(event);
-				this.notify('afterCallback',event);
-			}
-		}.bind(this);
-		this.enable();
-	},
-	trigger: function(){
-		this.handler();
-	},
-	enable: function(){
-		this.element.observe('keydown',this.handler);
-	},
-	disable: function(){
-		this.element.stopObserving('keydown',this.handler);
-	},
-	destroy: function(){
-		this.disable();
-		HotKey.hotkeys = HotKey.hotkeys.without(this);
-	}
+        this.element = $(this.options.element || document);
+        this.handler = function(event){
+            if(!event || (
+                (Event['KEY_' + this.letter] || this.letter.charCodeAt(0)) == event.keyCode &&
+                ((!this.options.shiftKey || (this.options.shiftKey && event.shiftKey)) &&
+                    (!this.options.altKey || (this.options.altKey && event.altKey)) &&
+                    (!this.options.ctrlKey || (this.options.ctrlKey && event.ctrlKey))
+                )
+            )){
+                if(this.notify('beforeCallback',event) === false) {
+                    return; }
+                this.callback(event);
+                this.notify('afterCallback',event);
+            }
+        }.bind(this);
+        this.enable();
+    },
+    trigger: function(){
+        this.handler();
+    },
+    enable: function(){
+        this.element.observe('keydown',this.handler);
+    },
+    disable: function(){
+        this.element.stopObserving('keydown',this.handler);
+    },
+    destroy: function(){
+        this.disable();
+        HotKey.hotkeys = HotKey.hotkeys.without(this);
+    }
 });
 Object.extend(HotKey,{
-	hotkeys: []
+    hotkeys: []
 });
 Object.Event.extend(HotKey);
